5fb9b02132439f2d98d4afb8ca8375395fa2cd4d
Fardeen Khan <khan.fardeen.dhn@gmail.com>
2024-11-23 22:05:59 +0530

    adding dummy data to DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const url =
-	"mongodb+srv://fardeenDev:nBiYoOu73by2H5PN@nodecoursecluster.7tn89.mongodb.net/";
+	"mongodb+srv://fardeenDev:nBiYoOu73by2H5PN@nodecoursecluster.7tn89.mongodb.net/DevFinder-DB";
 
 const connectDB = async () => {
 	try {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
+const User = require("./models/user");
 const app = express();
 
 connectDB()
@@ -9,3 +9,18 @@ connectDB()
 	)
 	.catch((err) => console.log(err.message));
 
+app.post("/signup", async (req, res) => {
+	const userDocument = new User({
+		name: "Fardeen",
+		age: 23,
+		gender: "male",
+		email: "fardeen@.com",
+	});
+	try {
+		await userDocument.save();
+		res.send("user created");
+	} catch (err) {
+		console.log("user creation failed");
+		res.send(err.message);
+	}
+});
